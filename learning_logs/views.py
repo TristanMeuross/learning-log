526ad80f1b6d07bd91ea06a126782ace773de082
TristanMeuross <61038321+TristanMeuross@users.noreply.github.com>
2025-07-21 22:03:54 +1000

    Added topics page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 
 def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.xhtml')
+
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.xhtml', context)
